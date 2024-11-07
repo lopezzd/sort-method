1d81ee91f59d5b394d212b118fb9d9947172a0f6
João Henrique Lopes <joaohenriquelopesdiv@gmail.com>
2024-11-07 13:11:54 -0300

    Inseretion  Sort

--- insertionSort.c
+++ insertionSort.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <locale.h>
 
-// Função de ordenação Insertion Sort
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
-        int key = arr[i]; // O elemento a ser inserido
+        int key = arr[i];
         int j = i - 1;
 
-        // Move os elementos do arr[0..i-1], que são maiores que a chave,
-        // para uma posição à frente de sua posição atual
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
-        arr[j + 1] = key; // Insere a chave na posição correta
+        arr[j + 1] = key; 
     }
 }
 
-// Função para imprimir o array
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
